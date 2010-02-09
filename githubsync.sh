5eb5bc43f2877c210cdd285e84cdaaa24e11e700
James Wilson <github@meswilson.com>
2010-02-09 01:00:01 -0600

    Automatic Push - Tue Feb  9 01:00:01 2010

--- githubsync.sh
+++ githubsync.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# automatically adds and syncs everything in this folder
+# uses this crontab entry
+# */5 * * * *  cd /home/james/Dropbox/code/ && ./githubsync.sh 
+
 printf "Adding files to index..."
 git add *
 git commit -m "Automatic Push - `date +%c`"
