c36df8629c273094c41f3a0e3efb74ecab244bc6
James Wilson <github@meswilson.com>
2010-02-10 03:20:02 -0600

    Automatic Push - Wed Feb 10 03:20:02 2010

--- homepage/index.php
+++ homepage/index.php
@@ -5,11 +5,27 @@
 
 </head>
 <body>
+<style type="text/css">
+#main {
+	font-size: 100%;
+}
 
+#header {
+	border: 3px;
+	-webkit-border-radius: 5px;
+	-moz-border-radius: 5px;
+	text-align: center;
+}
 
+.title {
+	font-size: 150%;
+}
+</style>
 <div id="main">
 	<div id="header">
-		
+		<span class="title">
+			James Wilson
+		</span>
 	</div>
 
 </div>
